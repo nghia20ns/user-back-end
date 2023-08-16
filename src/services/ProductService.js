fff42f56e0a88f371982108f688126fb69346f77
nghia20ns <quocnghia91ll@.com>
2023-08-16 16:00:48 +0700

    update

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -32,7 +32,6 @@ export const createManyProductService = async (apiInput) => {
         });
       }
     } catch (error) {
-      console.log(error.name);
       reject({
         status: "err",
         message: error,
